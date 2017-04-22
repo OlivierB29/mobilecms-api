dc958f1f145808e291bf9926feda38e129b2c75e
Olivier <olivier_boniteau@yahoo.fr>
2017-04-22 18:55:56 +0200

    JSON utility

--- src/api/v1/utils/JsonUtils.php
+++ src/api/v1/utils/JsonUtils.php
@@ -2,260 +2,104 @@
 include_once 'StringUtils.php';
 
 /**
- * Utilitaire JSON en PHP
+ * JSON utility for PHP
  */
-class JsonUtils {
-	public function __construct() {
-	}
-	public static function filterArray($data, $name, $value) {
-		$result = array ();
+class JsonUtils
+{
+    public function __construct()
+    {
+    }
 
-		if (isset ( $name ) && isset ( $value )) {
-			foreach ( $data as $element ) {
-				if ($element->{$name} == $value) {
-					array_push ( $result, $element );
-				}
-			}
-		} else {
-			$result = $data;
-		}
 
-		return $result;
-	}
-	public static function filterStartsWith($data, $name, $value) {
-		$result = array ();
 
-		if (isset ( $name ) && isset ( $value )) {
-			foreach ( $data as $element ) {
-				if (StringUtils::startsWith ( $element->{$name}, $value )) {
-					array_push ( $result, $element );
-				}
-			}
-		} else {
-			$result = $data;
-		}
+    /**
+    * @return JSON object (array or stdClass)
+    */
+    public static function readJsonFile(string $file)
+    {
 
-		return $result;
-	}
+        return json_decode(file_get_contents($file));
+    }
 
-	/*
-	 * TODO
-	 */
-	public static function filterArrayByJson($data, $object) {
-		foreach ( $object as $name => $value ) {
 
-			if (isset ( $name ) && isset ( $value )) {
-				$data = JsonUtils::filterArray ( $data, $name, $value );
-			}
-		}
+    /*
+    * write JSON object (array or stdClass)
+    */
+    public static function writeJsonFile(string $file, $data)
+    {
+        $fh = fopen($file, 'w') or die("Error opening output file" . $file);
+        fwrite($fh, json_encode($data, JSON_PRETTY_PRINT));
+        fclose($fh);
+    }
 
-		return $data;
-	}
-	public static function readJsonFile($file) {
 
-		
-		$str_data = file_get_contents ( $file );
-		$data = json_decode ( $str_data );
-		return $data;
-	}
-	public static function writeJsonFile($file, $data) {
-		$fh = fopen ( $file, 'w' ) or die ( "Error opening output file" );
-		fwrite ( $fh, json_encode ( $data, JSON_PRETTY_PRINT ) );
-		fclose ( $fh );
-	}
-	public static function updateObject($data, $object) {
-		if (isset ( $data ) && isset ( $object )) {
-			foreach ( $data as $element ) {
-				// getByKey
-				if ($element->{$keyname} == $object->{$keyname}) {
-					JsonUtils::copy ( $object, $element );
+
+
+    /**
+    * find a JSON object into a JSON array, by key=value
+    *
+    */
+    public static function getByKey(array $data, string  $name, string  $value)
+    {
+        $result = null;
+
+        if (isset($name) && isset($value)) {
+            foreach ($data as $element) {
+                if ($element->{$name} == $value) {
+                    $result = $element;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+		/**
+		* If the JSON array previously contained a mapping for the key,
+		* the old value is replaced by the specified value.
+		* @return updated array
+		*/
+		public static function put(array $data, string $name, stdClass $item) : array
+		{
+				$existing = JsonUtils::getByKey($data, $name, $item->{$name});
+
+				if(isset($existing)) {
+					JsonUtils::replace($item, $existing);
+				}	else {
+					array_push($data, $item);
 				}
 
-				array_push ( $result, $element );
-			}
-
-			/*
-			 * TODO
-			 * $element = JsonUtils::getByKey($data, $keyname, $keyvalue);
-			 * JsonUtils::copy($object, $element);
-			 */
-		} else {
-			$result = $data;
+				return $data;
 		}
 
-		return $result;
-	}
+    /**
+    * copy properties of $source to $dest, without including the new properties
+    * eg:
+    *	$source = {"id":"1", "foo":"pub" , "hello":"world"}
+    * $dest = {"id":"1", "foo":"bar"}
+    * --> $dest = {"id":"1", "foo":"pub"}
+    *
+    */
+    public static function copy(stdClass $source, stdClass  $dest)
+    {
+        foreach ($dest as $key => $value) {
+            $dest->{$key} = $source->{$key};
+        }
+    }
 
-	/*
-	 * TODO
-	 */
-	public static function updateByKey($data, $object, $keyname, $keyvalue) {
-		$result = array ();
+    /**
+    * copy properties of $source to $dest, including the new properties
+    * eg:
+    *	$source = {"id":"1", "foo":"pub" , "hello":"world"}
+    * $dest = {"id":"1", "foo":"bar"}
+    * --> $dest = {"id":"1", "foo":"pub" , "hello":"world"}
+    *
+    */
+    public static function replace(stdClass $source, stdClass  $dest)
+    {
+        foreach ($source as $key => $value) {
+            $dest->{$key} = $source->{$key};
+        }
+    }
 
-		if (isset ( $keyname ) && isset ( $keyvalue ) && isset ( $object )) {
-			foreach ( $data as $element ) {
-				// getByKey
-				if ($element->{$keyname} == $object->{$keyname}) {
-					JsonUtils::copy ( $object, $element );
-				}
-
-				array_push ( $result, $element );
-			}
-
-			/*
-			 * TODO
-			 * $element = JsonUtils::getByKey($data, $keyname, $keyvalue);
-			 * JsonUtils::copy($object, $element);
-			 */
-		} else {
-			$result = $data;
-		}
-
-		return $result;
-	}
-	public static function updateByIndex($data, $object, $index) {
-		$result = array ();
-
-		if (isset ( $data ) && isset ( $object ) && isset ( $index )) {
-			$count = 0;
-			foreach ( $data as $element ) {
-				// getByIndex ?
-				if ($count == $index) {
-					JsonUtils::copy ( $object, $element );
-				}
-
-				array_push ( $result, $element );
-
-				$count ++;
-			}
-
-			/*
-			 * TODO
-			 * $element = JsonUtils::getByKey($data, $keyname, $keyvalue);
-			 * JsonUtils::copy($object, $element);
-			 */
-		} else {
-			$result = $data;
-		}
-
-		return $result;
-	}
-	public static function getByKey($data, $name, $value) {
-		$result = null;
-
-		if (isset ( $name ) && isset ( $value )) {
-			foreach ( $data as $element ) {
-				if ($element->{$name} == $value) {
-					$result = $element;
-				}
-			}
-		}
-
-		return $result;
-	}
-	public static function getByIndex($data, $index) {
-		$result = null;
-		$count = 0;
-
-		if (isset ( $data ) && isset ( $index )) {
-			foreach ( $data as $element ) {
-				if ($index == $count) {
-					$result = $element;
-				}
-
-				$count ++;
-			}
-		}
-
-		return $result;
-	}
-	public static function copy($source, $dest) {
-		foreach ( $source as $prop => $val ) {
-
-			$dest->{$prop} = $source->{$prop};
-		}
-	}
-	public static function setProperty($data, $name, $value) {
-		foreach ( $data as $prop => $val ) {
-
-			if ($prop == $name) {
-
-				$dest->{$prop} = $value;
-			}
-		}
-	}
-	public static function sortByDateAsc($data) {
-		usort ( $data, function ($a, $b) { // Sort the array using a user defined function
-			return JsonUtils::intValue ( $b->{'date'} ) > JsonUtils::intValue ( $a->{'date'} ) ? - 1 : 1; // Compare the scores
-		} );
-
-		return $data;
-	}
-	public static function sortByDateDesc($data) {
-		usort ( $data, function ($a, $b) { // Sort the array using a user defined function
-			return JsonUtils::intValue ( $a->{'date'} ) > JsonUtils::intValue ( $b->{'date'} ) ? - 1 : 1; // Compare the scores
-		} );
-
-		return $data;
-	}
-	public static function getFeedByDate($data, $maxElements) {
-		usort ( $data, function ($a, $b) { // Sort the array using a user defined function
-			return JsonUtils::intValue ( $b->{'date'} ) > JsonUtils::intValue ( $a->{'date'} ) ? - 1 : 1; // Compare the scores
-		} );
-
-		// sample : 20161031
-		$today = JsonUtils::intValue ( date ( '%Y%m%d' ) );
-
-		// filter a max number of elements
-		$result = array ();
-
-		$i = 0;
-		while ( $i ++ < count ( $data ) && count ( $result ) <= $maxElements ) {
-
-			if (isset ( $data [$i]->{'date'} ) && JsonUtils::intValue ( $data [$i]->{'date'} ) >= $today) {
-				array_push ( $result, $data [$i] );
-			}
-		}
-
-		return $result;
-	}
-	public static function printDebug($element) {
-		$result = '';
-
-		foreach ( $element as $prop => $val ) {
-			$result .= $prop . ' : ' . $val;
-			$result .= '<br/>';
-		}
-
-		return $result;
-	}
-
-	/**
-	 *
-	 * @param string $date
-	 *        	(YYYY/MM/DD YYYY-MM ... )
-	 * @return string
-	 */
-	public static function intValue($date) {
-		$result = 0;
-
-		if (! empty ( $date )) {
-
-			$str = preg_replace ( "/[^0-9]/", "", $date );
-
-			// 201509 --> 20150900
-			while ( strlen ( $str ) < 8 ) {
-				$str .= '0';
-			}
-
-			if (! empty ( $str )) {
-
-				$result = intval ( $str );
-			}
-		}
-
-		return $result;
-	}
 }
-
-?>
--- tests/utils/JsonUtilsTest.php
+++ tests/utils/JsonUtilsTest.php
@@ -6,19 +6,156 @@ use PHPUnit\Framework\TestCase;
 
 final class JsonUtilsTest extends TestCase
 {
+
     public function testCanRead()
-    {
-        $this->assertTrue(
-          JsonUtils::readJsonFile('tests-data/jsonutils/test.json') !== null
-        );
-    }
-
-
-    public function testCanBeUsedAsString()
     {
         $this->assertJsonStringEqualsJsonString(
            '{}',
-            json_encode(JsonUtils::readJsonFile('tests-data/jsonutils/test.json'))
+            json_encode(JsonUtils::readJsonFile('tests-data/jsonutils/mini.json'))
         );
     }
+
+
+
+    public function testGetByKey()
+    {
+
+        $data = JsonUtils::readJsonFile('tests-data/jsonutils/test.json');
+
+        $item = JsonUtils::getByKey($data, 'id', '1');
+
+
+        $this->assertJsonStringEqualsJsonString(
+        '{"id":"1", "foo":"bar"}',
+        json_encode($item)
+
+      );
+    }
+
+    public function testUpdateByKey()
+    {
+
+
+        $data = JsonUtils::readJsonFile('tests-data/jsonutils/test.json');
+
+        $item = JsonUtils::getByKey($data, 'id', '1');
+
+        $item->{'foo'} = 'pub';
+
+
+        $this->assertJsonStringEqualsJsonString(
+        '{"id":"1", "foo":"pub"}',
+        json_encode($item)
+      );
+
+      $this->assertJsonStringEqualsJsonString(
+      '[{"id":"1", "foo":"pub"},{"id":"2", "foo":"bar"}]',
+      json_encode($data)
+    );
+    }
+
+    public function testCopy()
+    {
+
+
+        $data = JsonUtils::readJsonFile('tests-data/jsonutils/test.json');
+
+        $item = JsonUtils::getByKey($data, 'id', '1');
+
+        $this->assertJsonStringEqualsJsonString(
+        '{"id":"1", "foo":"bar"}',
+        json_encode($item)
+      );
+
+        $newItem = json_decode('{"id":"1", "foo":"pub"}');
+
+        $this->assertTrue(
+        $newItem != null
+      );
+
+        JsonUtils::copy($newItem, $item);
+
+
+
+
+        $this->assertJsonStringEqualsJsonString(
+        '{"id":"1", "foo":"pub"}',
+        json_encode($item)
+      );
+
+      $this->assertJsonStringEqualsJsonString(
+      '[{"id":"1", "foo":"pub"},{"id":"2", "foo":"bar"}]',
+      json_encode($data)
+    );
+    }
+
+
+    public function testReplace()
+    {
+
+
+        $data = JsonUtils::readJsonFile('tests-data/jsonutils/test.json');
+
+        $item = JsonUtils::getByKey($data, 'id', '1');
+
+        $this->assertJsonStringEqualsJsonString(
+        '{"id":"1", "foo":"bar"}',
+        json_encode($item)
+      );
+
+        $newItem = json_decode('{"id":"1", "foo":"pub" , "hello":"world"}');
+
+        $this->assertTrue(
+        $newItem != null
+      );
+
+        JsonUtils::replace($newItem, $item);
+
+
+
+
+        $this->assertJsonStringEqualsJsonString(
+        '{"id":"1", "foo":"pub", "hello":"world"}',
+        json_encode($item)
+      );
+
+      $this->assertJsonStringEqualsJsonString(
+      '[{"id":"1", "foo":"pub", "hello":"world"},{"id":"2", "foo":"bar"}]',
+      json_encode($data)
+    );
+    }
+
+    public function testPutExistingItem()
+    {
+
+
+        $data = JsonUtils::readJsonFile('tests-data/jsonutils/test.json');
+
+        $item = json_decode('{"id":"1", "foo":"pub"}');
+        JsonUtils::put($data, 'id', $item);
+
+
+
+      $this->assertJsonStringEqualsJsonString(
+      '[{"id":"1", "foo":"pub"},{"id":"2", "foo":"bar"}]',
+      json_encode($data)
+    );
+    }
+
+    public function testPutNewItem()
+    {
+
+
+        $data = JsonUtils::readJsonFile('tests-data/jsonutils/test.json');
+
+        $item = json_decode('{"id":"100", "foo":"bar"}');
+        $data = JsonUtils::put($data, 'id', $item);
+
+
+      $this->assertJsonStringEqualsJsonString(
+      '[{"id":"1", "foo":"bar"},{"id":"2", "foo":"bar"},{"id":"100", "foo":"bar"}]',
+      json_encode($data)
+    );
+    }
+
 }
