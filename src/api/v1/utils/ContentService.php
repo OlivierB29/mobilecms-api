8ae2af7d6ae8011e438a9b2ece526ca99557197d
Olivier <olivier_boniteau@yahoo.fr>
2017-04-22 16:55:46 +0200

    content service

--- src/api/v1/utils/ContentService.php
+++ src/api/v1/utils/ContentService.php
@@ -3,150 +3,237 @@ require_once 'Response.php';
 require_once 'JsonUtils.php';
 
 /**
- * Save an object and constructs a response
+ * Read and save data from JSON files.
+ * Future plans : consider http://stackoverflow.com/questions/13899342/can-we-use-json-as-a-database
+ * Public methods :
+ * - getAll : get all elements
+ * - get : return a single element
+ * - put : create a new item
+ * - post : update an existing item
  */
-class ContentService {
+class ContentService
+{
+
+
 
 	/**
-	 * $dir : database Directory
-	 * $filename : JSON data filename
-	 * $keyname : primary key inside the file
+	 * main directory (eg: /opt/foobar/data )
 	 */
-	public function get($dir, $filename, $keyname) {
-		$response = new Response ();
-		$response->setCode ( 400 );
-		$response->setMessage ( "Bad parameters" );
-		$response->setResult ( "{}" );
+	private $databasedir;
 
-		try {
+	function __construct($databasedir) {
+		$this->databasedir = $databasedir;
+	}
 
-			// Read the JSON file
-			$file = $dir . '/' . $filename;
-			$data = JsonUtils::readJsonFile ( $file );
+    /**
+    * return a single element
+     * $filename : JSON data filename
+     * $keyname : primary key inside the file
+     * @return : Response object with a JSON object
+     */
+    public function get( string $filename, string $keyname, string $keyvalue)
+    {
+        $response = new Response();
+        $response->setCode(400);
+        $response->setMessage("Bad parameters");
+        $response->setResult("{}");
 
-			// get one element
-			if (isset ( $_GET ["id"] )) {
-				$keyvalue = $_GET ["id"];
+        try {
 
-				// extract element data
-				$existingObject = JsonUtils::getByKey ( $data, $keyname, $keyvalue );
-				if (isset ( $existingObject )) {
+            // Read the JSON file
+            $file = $this->databasedir . '/' . $filename;
+            $data = JsonUtils::readJsonFile($file);
 
-					$response->setResult ( $existingObject );
-					$response->setCode ( 200 );
-				} else {
-					// element not found
-					$response->appendMessage ( 'not found ' . $keyname . ' : ' . $keyvalue );
-					$response->setCode ( 404 );
+            // get one element
+            if (isset($keyvalue)) {
+
+                // extract element data
+                $existingObject = JsonUtils::getByKey($data, $keyname, $keyvalue);
+                if (isset($existingObject)) {
+                    $response->setResult(json_encode($existingObject));
+                    $response->setCode(200);
+                } else {
+                    // element not found
+                    $response->appendMessage('not found ' . $keyname . ' : ' . $keyvalue);
+                    $response->setCode(404);
+                }
+            } else {
+                // return all
+                $response->setResult(json_encode($data));
+                $response->setCode(200);
+            }
+        } catch (Exception $e) {
+            $response->setCode(520);
+            $response->setMessage($e->getMessage());
+        } finally {
+						return $response;
+        }
+    }
+
+    /**
+    * get all elements
+     * $filename : JSON data filename
+     * $keyname : primary key inside the file
+     * @return : Response object with a JSON array
+     */
+    public function getAll( string $filename)
+    {
+        $response = new Response();
+        $response->setCode(400);
+        $response->setMessage("Bad parameters");
+        $response->setResult('{}');
+
+        try {
+
+            // Read the JSON file
+            $file = $this->databasedir . '/' . $filename;
+            $data = JsonUtils::readJsonFile($file);
+            if (isset($data)) {
+                $response->setCode(200);
+                $response->setResult(json_encode($data));
+            }
+        } catch (Exception $e) {
+            $response->setCode(520);
+            $response->setMessage($e->getMessage());
+            $response->setResult('{ "result":"' . $e->getMessage() .'"}');
+        } finally {
+            return $response;
+        }
+    }
+
+    /**
+     * create a single element
+     * $type : object type (eg : calendar)
+     * $filename : JSON data filename
+     * $keyname : primary key inside the file
+     * @return : Response object with a JSON object
+     */
+    public function putById( string $type, string $keyname, string $recordStr)
+    {
+        //TODO : throw an exception if existing file
+        return $this->postById($this->databasedir, $type, $keyname, $recordStr);
+    }
+
+		public function postItem( string $type, string $keyname, string $recordStr)
+		{
+			$response = new Response();
+			$response->setCode(400);
+			$response->setMessage("Bad parameters");
+			$response->setResult("{}");
+
+				try {
+						if (isset($recordStr)) {
+
+						//Decode JSON (and check if correct structure)
+						$myobjectJson = json_decode($recordStr);
+
+						//detect id
+						$id = $myobjectJson->{$keyname};
+
+
+						//create file name
+						$file = $this->databasedir . '/' . $type . '/' . $id . '.json';
+            JsonUtils::writeJsonFile($file, $myobjectJson);
+						$response->setCode(200);
+
+						} else {
+								$response->setMessage("Bad parameters : id, object");
+						}
+				} catch (Exception $e) {
+						$response->setCode(520);
+						$response->setMessage($e->getMessage());
+				} finally {
+						return $response;
 				}
-			} else {
-				// return all
-				$response->setResult ( $data );
-				$response->setCode ( 200 );
-			}
-		} catch ( Exception $e ) {
-			$response->setCode ( 520 );
-			$response->setMessage ( $e->getMessage () );
-		} finally {
-
-			http_response_code ( $response->getCode () );
-			echo json_encode ( $response->getResult () );
-		}
-	}
-	public function getAll($dir, $filename) {
-		$response = new Response ();
-		$response->setCode ( 400 );
-		$response->setMessage ( "Bad parameters" );
-		$response->setResult ( '{}' );
-
-		try {
-
-			// Read the JSON file
-			$file = $dir . '/' . $filename;
-			$data = JsonUtils::readJsonFile ( $file );
-			if (isset ( $data )) {
-				$response->setCode ( 200 );
-				$response->setResult ( $data );
-			}
-		} catch ( Exception $e ) {
-			$response->setCode ( 520 );
-			$response->setMessage ( $e->getMessage () );
-			$response->setResult ( '{ "result":"' . $e->getMessage () .'"}');
-		} finally {
-
-			return $response;
-		}
-	}
-	public function post($dir, $type, $keyname, $recordStr) {
-		$response = new Response ();
-		$response->setCode ( 400 );
-		$response->setMessage ( "Bad parameters" );
-		$response->setResult ( "{}" );
-
-		try {
-
-			if (isset ( $recordStr )) {
-
-				$responseSave = $this->save ( $dir, $type, $keyname, $recordStr );
-
-				if (isset ( $responseSave )) {
-					$response = $responseSave;
-				}
-			} else {
-				$response->setMessage ( "Bad parameters : id, object" );
-			}
-		} catch ( Exception $e ) {
-			$response->setCode ( 520 );
-			$response->setMessage ( $e->getMessage () );
-		} finally {
-
-		return $response;
-		}
-	}
-	public function save($dir, $type, $keyname, $recordStr) {
-		$response = new Response ();
-		$response->setCode ( 400 );
-		$response->setMessage ( "" );
-		$response->setResult ( '{"save" : "' . $dir . ' ' . $type . ' ' . $keyname . ' ' . $recordStr . '"}' );
-
-		if (isset ( $dir ) && isset ( $type ) && isset ( $keyname )) {
-			$response->setResult ( "isset OK" );
-
-			$myobjectJson = json_decode ( $recordStr );
-
-			$id = $myobjectJson->{$keyname};
-
-			$response->setResult ( "json_decode OK " . $id );
-
-			// Read the JSON file
-			$file = $dir . '/' . $type . '/' . $id . '.json';
-
-			// $file = '/public/' . $type . '/' . $id . '.json' ;
-
-			$response->setResult ( "file OK " . $file );
-			$data = JsonUtils::readJsonFile ( $file );
-
-			if (isset ( $data )) {
-
-				// debug
-				$response->appendMessage ( 'debugging ...' );
-
-				JsonUtils::writeJsonFile ( $file, $recordStr );
-
-				$response->appendMessage ( $file . " : saved, " );
-
-				$response->setResult ( 'true' );
-				$response->setCode ( 200 );
-			} else {
-				$response->appendMessage ( 'not found ' . $id );
-				$response->setCode ( 404 );
-			}
-		} else {
-			$response->appendMessage ( 'empty params !' );
 		}
 
-		return $response;
-	}
+    /**
+     * basic controls and save a single element
+     * $this->databasedir : database Directory
+     * $type
+     * $filename : JSON data filename
+     * $keyname : primary key inside the file
+     * @return : Response object with a JSON object
+     */
+    public function postById( string $type, string $keyname, string $recordStr)
+    {
+        $response = new Response();
+        $response->setCode(400);
+        $response->setMessage("Bad parameters");
+        $response->setResult("{}");
+
+        try {
+            if (isset($recordStr)) {
+                $responseSave = $this->save($type, $keyname, $recordStr);
+
+                if (isset($responseSave)) {
+                    $response = $responseSave;
+                }
+            } else {
+                $response->setMessage("Bad parameters : id, object");
+            }
+        } catch (Exception $e) {
+            $response->setCode(520);
+            $response->setMessage($e->getMessage());
+        } finally {
+            return $response;
+        }
+    }
+    /**
+     * TODO : save a single element, into a JSON array file. eg : [ {"id":"1", "foo":"bar"}, {"id":"2", "foo":"bar"} ]
+     * $type : name of object (and subdirectory)
+     * $filename : JSON data filename
+     * $keyname : primary key inside the file
+     * @return : Response object with a JSON object
+     */
+    private function save( string $type, string $keyname, string $recordStr)
+    {
+        $response = new Response();
+        $response->setCode(400);
+        //$response->setMessage('{"save" : "' . $this->databasedir . ' ' . $type . ' ' . $keyname . ' ' . $recordStr . '"}');
+				$response->setMessage('');
+        $response->setResult('{}');
+
+        if (isset($this->databasedir) && isset($type) && isset($keyname)) {
+            $response->setResult("isset OK");
+
+            $myobjectJson = json_decode($recordStr);
+
+            $id = $myobjectJson->{$keyname};
+
+            $response->setResult("json_decode OK " . $id);
+
+            // Read the JSON file
+            $file = $this->databasedir . '/' . $type . '/' . $id . '.json';
+
+            // $file = '/public/' . $type . '/' . $id . '.json' ;
+
+            $response->setResult("file OK " . $file);
+            $data = JsonUtils::readJsonFile($file);
+
+            if (isset($data)) {
+
+                // debug
+                $response->appendMessage('debugging ...');
+
+                JsonUtils::writeJsonFile($file, $recordStr);
+
+                $response->appendMessage($file . " : saved, ");
+
+
+                $response->setCode(200);
+            } else {
+                $response->appendMessage('not found ' . $id);
+                $response->setCode(404);
+            }
+        } else {
+            $response->appendMessage('empty params !');
+        }
+
+        return $response;
+    }
+
+
+
+
 }
-
-?>
--- tests/utils/ContentServiceTest.php
+++ tests/utils/ContentServiceTest.php
@@ -5,9 +5,55 @@ use PHPUnit\Framework\TestCase;
 
 final class ContentServiceTest extends TestCase
 {
-    public function testOk()
-    {
+    private $dir = 'tests-data/public';
 
+    public function testGetAll()
+    {
+        $service = new ContentService($this->dir);
+        $response = $service->getAll('calendar/index.json');
+
+
+        $this->assertEquals($response->getCode(), 200);
+
+        $this->assertTrue(
+          strstr($response->getResult(), '"id":"1"') != ''
+        );
+
+
+        $this->assertTrue(
+          strstr($response->getResult(), '"id":"2"') != ''
+        );
+
+
+    }
+
+    public function testGetItemFromList()
+    {
+        $service = new ContentService($this->dir);
+        $response = $service->get('calendar/index.json', 'id', '1');
+
+        $this->assertEquals($response->getCode(), 200);
+
+        $this->assertJsonStringEqualsJsonString(
+        json_encode(json_decode('{"id":"1","date":"201509","activity":"activitya","title":"some seminar of activity A","organization":"Some org","description":"some infos","url":"","location":"","startdate":"","enddate":"","updated":"","updatedby":""}')),
+        $response->getResult()
+      );
+    }
+
+    public function testSaveItem()
+    {
+        $recordStr = '{"id":"10","date":"201509","activity":"activitya","title":"some seminar of activity A","organization":"Some org","description":"some infos","url":"","location":"","startdate":"","enddate":"","updated":"","updatedby":""}';
+        $service = new ContentService($this->dir);
+        $response = $service->postItem('calendar', 'id', $recordStr);
+
+        $file = $this->dir . '/calendar/10.json';
+
+        $this->assertEquals($response->getCode(), 200);
+
+
+        $this->assertJsonStringEqualsJsonFile(
+            $file , $recordStr
+        );
     }
 
 
