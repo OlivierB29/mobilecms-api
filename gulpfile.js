b10dda78cd139bc08acb25c8e45fda5471939c81
Olivier <olivier_boniteau@yahoo.fr>
2017-05-03 22:08:59 +0200

    PHPUnit tests

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+
+var basedir = '.';
+
+var projectName = 'adminapp';
+
+var serverDeployDir = '/var/www/html';
+
+/**
+ * deploy php server code to local web server
+ */
+gulp.task('deploy', function() {
+
+  var srcPath = basedir + '/src/**';
+  var destPath = serverDeployDir + '/' + projectName;
+
+  console.log('Deploying server : ' + srcPath + ' --> ' + destPath);
+
+  gulp.src(srcPath).pipe(gulp.dest(destPath));
+
+});
+
+/*
+* Run tests
+* Requirements : phpunit
+* https://phpunit.de/getting-started.html
+*/
+gulp.task('test-utils', function() {
+
+  console.log('phpunit tests :');
+
+
+
+  var run = require('gulp-run');
+
+  return run('phpunit --configuration phpunit-utils.xml --testsuite utils').exec()
+    .pipe(gulp.dest('output'))
+  ;
+
+});
+
+
+gulp.task('default', function() {
+
+  console.log('Server : check directories, and then \n gulp deploy');
+
+
+});
