4ab141140999f62f22863da0c7a4ff8ff8165527
Olivier <olivier_boniteau@yahoo.fr>
2017-07-24 22:03:48 +0200

    file upload API

---
+++ src/api/v1/fileapi.php
@@ -0,0 +1,35 @@
+<?php
+include 'conf.php';
+include 'utils/FileApi.php';
+
+// cross domain !
+if (null !== ALLOW_CROSS_DOMAIN && ALLOW_CROSS_DOMAIN === 'true') {
+    header('Access-Control-Allow-Origin: *');
+}
+
+try {
+    $conf = json_decode('{"enableheaders" : "","enableapikey" : "true", "publicdir":"", "privatedir":"" , "apikeyfile" : "" }');
+    $conf->{'enableheaders'} = 'true';
+    $conf->{'enableapikey'} = 'false';
+    $conf->{'homedir'} = HOME;
+    $conf->{'media'} = 'media';
+    $conf->{'privatedir'} = PRIVATEDIR;
+
+    $API = new FileApi($conf);
+
+
+    $API->setRequest();
+    $API->authorize();
+
+    echo $API->processAPI();
+
+} catch (Exception $e) {
+    echo json_encode([
+            'error' => $e->getMessage(),
+    ]);
+}
+
+
+
+
+?>
---
+++ src/api/v1/utils/FileApi.php
@@ -0,0 +1,179 @@
+<?php
+
+require_once 'SecureRestApi.php';
+
+/*
+ * File API with authentication.
+ * Basic file upload using _FILES
+ */
+class FileApi extends SecureRestApi
+{
+
+  /**
+   * media directory (eg: media ).
+   */
+  private $media;
+
+  private $homedir;
+
+  /**
+   * media directory (eg: /var/www/html/media ).
+   */
+  private $mediadir;
+
+    public function __construct($conf)
+    {
+        parent::__construct($conf);
+
+        // Default headers for RESTful API
+        if ($this->enableHeaders) {
+            header('Access-Control-Allow-Methods: *');
+        }
+        $this->homedir =  $this->conf->{'homedir'};
+
+        $this->media = $this->conf->{'media'};
+
+        $this->mediadir = $this->conf->{'homedir'} . '/' . $this->media;
+    }
+
+    /**
+    * basic file upload
+    */
+    protected function basicupload()
+    {
+        $response = new Response();
+        $response->setCode(400);
+        $response->setMessage('Bad parameters');
+        $response->setResult('{}');
+
+        try {
+            $this->checkConfiguration();
+
+            $datatype = $this->getDataType();
+
+          //
+          // Preflight requests are send by Angular
+          //
+          if ($this->method === 'OPTIONS') {
+              // eg : /api/v1/content
+              $response->setResult($this->preflight());
+          }
+
+          //
+          if (isset($datatype) && strlen($datatype) > 0) {
+              // eg : /api/v1/content/calendar
+              if ($this->method === 'GET') {
+                // TODO get file
+              } elseif ($this->method === 'POST') {
+
+                if (array_key_exists(0, $this->args)) {
+                    //get the full data of a single record $this->args contains the remaining path parameters
+                    // eg : /api/v1/file/calendar/1
+                    $uploadResult = $this->uploadFiles($datatype, $this->args[0]);
+                    $response->setCode(200);
+                    $response->setMessage('');
+                    $response->setResult(json_encode($uploadResult));
+
+                }
+
+
+              }
+          }
+        } catch (Exception $e) {
+            $response->setCode(520);
+            $response->setMessage($e->getMessage());
+            $response->setResult($this->errorToJson($e->getMessage()));
+        } finally {
+
+          return $response->getResult();
+        }
+    }
+
+
+
+    private function uploadFiles($type, $id)
+    {
+      /*
+      File properties example
+      - name:1.jpg
+      - type:image/jpeg
+      - tmp_name:/tmp/phpzDc6qT
+      - error:0
+      - size:700
+        */
+      $result = json_decode('[]');
+      foreach ($_FILES as $formKey => $file) {
+
+
+            // media/calendar/1
+            $uridir = $this->media. '/' . $type. '/' . $id  ;
+
+            // /var/www/html/media/calendar/1
+            $destdir = $this->homedir. '/' . $uridir;
+
+            // create directory if it doesn't exist
+            if (!file_exists($destdir)) {
+                mkdir($destdir, 0777, true);
+            }
+
+            // upload
+            if (isset($file['tmp_name']) && isset($file['name'])) {
+              $destfile = $destdir . '/' . $file['name'];
+              if(move_uploaded_file($file['tmp_name'], $destfile)) {
+
+                $finfo = finfo_open(FILEINFO_MIME_TYPE); // get mime type
+                $mimetype = finfo_file($finfo, $destfile);
+                finfo_close($finfo);
+
+                  $fileResult = json_decode('{}');
+                  $fileResult->{'title'} = $file['name'];
+                  $fileResult->{'url'} = '/' . $uridir . '/' . $file['name'];
+                  $fileResult->{'size'} = $file['size'];
+                  $fileResult->{'mimetype'} = $mimetype;
+                  array_push($result, $fileResult);
+              } else {
+                  throw new Exception($file['name'] . " KO");
+              }
+            }
+
+    }
+
+    if (count($result) === 0) {
+      throw new Exception("no files");
+    }
+
+    return $result;
+    }
+
+    private function getDataType(): string
+    {
+        $datatype = null;
+        if (isset($this->verb)) {
+            $datatype = $this->verb;
+        }
+        if (!isset($datatype)) {
+            throw new Exception('Empty datatype');
+        }
+
+        return $datatype;
+    }
+
+    private function checkConfiguration()
+    {
+        if (!isset($this->conf->{'homedir'})) {
+            throw new Exception('Empty publicdir');
+        }
+    }
+
+    /**
+     * http://stackoverflow.com/questions/25727306/request-header-field-access-control-allow-headers-is-not-allowed-by-access-contr.
+     */
+    public function preflight(): string
+    {
+        header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+        return '{}';
+    }
+}
+?>
