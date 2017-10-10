c131e5a258eb102b2cf70b7d369f8e58dec0fc8b
Olivier <olivier_boniteau@yahoo.fr>
2017-10-10 19:33:33 +0200

    JSON conf

--- config/development/conf.php
+++ config/development/conf.php
@@ -1,17 +1,3 @@
 <?php
 
-define('ERROR_LOG', 'true');
-
-// enable CORS
-define('ALLOW_CROSS_DOMAIN', 'true');
-
-// block HTTP if activated
-define('ACTIVATE_HTTPS', 'false');
-
-//eg : /var/www/html
-define('HOME', $_SERVER['DOCUMENT_ROOT']);
-
-//If possible, use a directory only accessible with filesystem queries.
-//Unless, use a .htaccess file
-//eg : /var/www/private
-define('PRIVATEDIR', realpath($_SERVER['DOCUMENT_ROOT'].'/../private'));
+// deprecated
