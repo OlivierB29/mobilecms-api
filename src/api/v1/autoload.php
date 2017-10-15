5016fb7aabd3dce2809f8bd530b587aa9e1ed64d
Olivier <olivier_boniteau@yahoo.fr>
2017-10-15 17:46:05 +0200

    PHP autoloader

---
+++ src/api/v1/autoload.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once 'autoloader/Autoloader.php';
+
+spl_autoload_register(['AutoLoader', 'loadClass']);
+
+// Register the directory to your include files
+AutoLoader::registerDirectory('utils');
