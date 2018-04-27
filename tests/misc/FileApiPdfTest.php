343ae4891b863fefac93d93467c86684fb6092a9
Olivier <olivier_boniteau@yahoo.fr>
2018-04-27 22:32:53 +0200

    pdf tests refactoring

---
+++ tests/misc/FileApiPdfTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+namespace mobilecms\api;
+
+use PHPUnit\Framework\TestCase;
+
+final class FileApiTest extends AuthApiTest
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->API=new FileApi();
+        $this->API->loadConf(realpath('tests/conf.json'));
+        $this->API->setRootDir(realpath('tests-data')); // unit test only
+        $this->setAdmin();
+    }
+
+    public function testUploadPdfFileAndThumbnails()
+    {
+        // API request
+        $record = '/calendar/3';
+        $this->path = '/fileapi/v1/basicupload' . $record;
+        $filename = 'testupload2.pdf';
+        // mock file
+        $mockUploadedFile = realpath('tests-data/fileapi/save/') . 'foobar123.pdf';
+        copy('tests-data/fileapi/save/' . $filename, $mockUploadedFile);
+        $files = [
+        ['name'=>$filename,'type'=>'application/pdf','tmp_name'=> $mockUploadedFile,'error'=>0,'size'=>24612]
+        ];
+
+        //
+        // UPLOAD
+        //
+        $this->SERVER = ['REQUEST_URI' => $this->path, 'REQUEST_METHOD' => 'POST', 'HTTP_ORIGIN' => 'foobar'];
+        $this->API->setDebug(true);
+        $this->API->setRequest($this->REQUEST, $this->SERVER, $this->GET, $this->POST, $this->headers, $files);
+        $this->API->authorize($this->headers, $this->SERVER);
+        $response = $this->API->processAPI();
+        $result = $response->getResult();
+        $this->printError($response);
+        $this->assertEquals(200, $response->getCode());
+        $this->assertTrue($result != null && $result != '');
+        $expected = '[{"title":"'.$filename.'","url":"'.$filename.'","size":163452,"mimetype":"application\/pdf"}]';
+        $this->assertJsonStringEqualsJsonString($expected, $result);
+
+
+        //
+        // THUMBNAILS
+        //
+        $this->path = '/fileapi/v1/thumbnails/calendar/3';
+        $this->SERVER = ['REQUEST_URI' => $this->path, 'REQUEST_METHOD' => 'POST', 'HTTP_ORIGIN' => 'foobar'];
+        $recordStr = '[{ "url": "'.$filename.'", "sizes": ["100", "200", "300"]}]';
+        $this->POST = ['requestbody' => $recordStr];
+        unset($recordStr);
+        $this->API->setRequest($this->REQUEST, $this->SERVER, $this->GET, $this->POST, $this->headers);
+        $this->API->authorize($this->headers, $this->SERVER);
+        $response = $this->API->processAPI();
+        $result = $response->getResult();
+        echo '!!!'.$result;
+        $this->assertEquals(200, $response->getCode());
+        $this->assertTrue($result != null && $result != '');
+        $expected = '[]';
+        $this->assertJsonStringEqualsJsonString($expected, $result);
+        $mediaFile = $this->API->getMediaDirPath() . $record . '/' . $filename;
+        $this->assertTrue(file_exists($mediaFile));
+        unlink($mediaFile);
+    }
+}
